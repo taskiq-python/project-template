37929d020f233e4d37338f1c1b04462660b62b29
Pavel Kirilin <win10@list.ru>
2023-10-12 12:41:53 +0400

    Updated template.
    Signed-off-by: Pavel Kirilin <win10@list.ru>

--- {{cookiecutter.project_name | replace('_', '-') }}/{{cookiecutter.project_name}}/__init__.py
+++ {{cookiecutter.project_name | replace('_', '-') }}/{{cookiecutter.project_name}}/__init__.py
@@ -1,14 +1,1 @@
 """Project was generated using https://github.com/taskiq-python/project-template/."""
-
-try:
-    # Python 3.8+
-    from importlib.metadata import version  # noqa: WPS433
-except ImportError:
-    # Python 3.7
-    from importlib_metadata import version  # noqa: WPS433
-
-__version__ = version("{{cookiecutter.project_name}}")
-
-__all__ = [
-    "__version__"
-]
